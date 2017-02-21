4d038a4d74a91a9f4189306decaedea264bd3ac7
Masaya Kazama <miyan@t-p.jp>
2017-02-21 22:00:55 +0900

    🐚 Use no name as default

--- src/chrome_extension/content/enabler.js
+++ src/chrome_extension/content/enabler.js
@@ -4,11 +4,9 @@ chrome.storage.onChanged.addListener(() => {
   location.reload()
 })
 
-export const enabler = (cb) => {
+export default (cb) => {
   chrome.storage.local.get(storeKey, (items) => {
     if (items[storeKey]) return
     cb()
   })
 }
-
-export default enabler
