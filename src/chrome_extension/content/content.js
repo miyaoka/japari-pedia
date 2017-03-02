f80f2a2ae41aff5e334873c909a7c3de75f856d1
Masaya Kazama <miyan@t-p.jp>
2017-03-03 01:04:51 +0900

    🐙 Making Japari-pedia more i18n friendly

--- src/chrome_extension/content/content.js
+++ src/chrome_extension/content/content.js
@@ -3,44 +3,39 @@ import './css/header.scss'
 import './css/content.scss'
 import './css/nav.scss'
 import './css/kemo.scss'
+import language from './language.yml'
 
-import language from './language.json';
-
-const words = (() => {
-  // Detect the language of the wiki page
-  switch (/https:\/\/(.*?).wiki/.exec(document.URL)[1]) {
-    case "en" : return language.en; break;
-    case "jp" : return language.jp; break;
-    default: return language.jp; break;
-  }
-})()
+const defaultLang = 'en'
+const lang = document.documentElement.lang
+const words = Object.prototype.hasOwnProperty.call(language, lang)
+? language[lang]
+: language[defaultLang]
 
 const sayRate = 0.3
 
 const randomPick = list => list[Math.floor(Math.random() * list.length)]
-
 const kemoInnerHTML = list => `<span class="kemo-say">＼${randomPick(list)}／</span>`
+const escapeRegExp = str => str.replace(/[\\^$.*+?()[\]{}|]/g, '\\$&')
 
 const japarizeWikipedia = () => {
-  const headPattern = 'wikipedia'
+  const wikipedia = 'wikipedia'
   const contextPatterns = [
-    'wikipedia',
-    'ウィキペディア',
+    wikipedia,
+    words.wikipedia,
   ]
-  const after = 'ジャパリ図書館'
 
   // document title
   document.title = document.title.replace(
-    new RegExp(`${headPattern}$`, 'i'),
-    after
+    new RegExp(`${wikipedia}$`, 'i'),
+    words.title
   )
 
   // search props
   const searchInput = document.getElementById('searchInput')
   const props = ['placeholder', 'title']
-  const searchRegex = new RegExp(headPattern, 'i')
+  const searchRegex = new RegExp(wikipedia, 'i')
   props.forEach((prop) => {
-    searchInput[prop] = searchInput[prop].replace(searchRegex, after)
+    searchInput[prop] = searchInput[prop].replace(searchRegex, words.title)
   })
 
   // text content
@@ -54,7 +49,7 @@ const japarizeWikipedia = () => {
   let n
   const contextRegex = new RegExp(`(${contextPatterns.join('|')})`, 'gi')
   while ((n = walker.nextNode())) {
-    n.textContent = n.textContent.replace(contextRegex, after)
+    n.textContent = n.textContent.replace(contextRegex, words.title)
   }
 }
 
@@ -75,10 +70,11 @@ const becomeFriendWithSectionTitle = () => {
 }
 
 const insertWordsInContent = () => {
+  const regex = new RegExp(`(.+?${escapeRegExp(words.delimiter)})(?!([^<]+)?>)`, 'g')
   document
   .querySelectorAll('#mw-content-text > p')
   .forEach((para) => {
-    let sentence = para.innerHTML.match(/(.+?[。]|.+?\. )(?!([^<]+)?>)/g)
+    let sentence = para.innerHTML.match(regex)
     if (!sentence) {
       return
     }
@@ -87,13 +83,13 @@ const insertWordsInContent = () => {
     sentence = sentence
     .map((s, i) => (
       (i !== last && Math.random() < sayRate)
-        ? s + ' ' + kemoInnerHTML(words.postPeriodList)
+        ? `${s}${kemoInnerHTML(words.postPeriodList)}`
         : s
       )
     )
 
     if (Math.random() < sayRate) {
-      sentence.push(' ' + kemoInnerHTML(words.postParaList))
+      sentence.push(kemoInnerHTML(words.postParaList))
     }
 
     para.innerHTML = sentence.join('')
